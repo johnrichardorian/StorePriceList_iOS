166a1fad4c478d2bb66fce9b986a02b3c3fa2765
Keifen Arroyo <arroyokarlkeifen@gmail.com>
2025-08-28 16:31:40 +0800

    Initial Commit

---
+++ StorePriceList/StorePriceListApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorePriceListApp.swift
+//  StorePriceList
+//
+//  Created by STUDENT on 8/28/25.
+//
+
+import SwiftUI
+
+@main
+struct StorePriceListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StorePriceListTests/StorePriceListTests.swift
@@ -0,0 +1,17 @@
+//
+//  StorePriceListTests.swift
+//  StorePriceListTests
+//
+//  Created by STUDENT on 8/28/25.
+//
+
+import Testing
+@testable import StorePriceList
+
+struct StorePriceListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
